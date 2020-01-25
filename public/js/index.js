d67e3aab2b4e1e63348bf38fae8ef94f681a1539
Frants Mitskun <frantsmn@users.noreply.github.com>
2020-01-26 02:19:03 +0300

    Update

--- public/js/index.js
+++ public/js/index.js
@@ -13,14 +13,22 @@ new Vue({
     },
 
     methods: {
+        onReset: function () {
+            this.isBusy = false;
+            this.textareaText = '';
+            this.domains = [];
+            this.items = [];
+        },
         onSubmit: async function () {
             this.isBusy = true;
             this.domains = this.textareaText
                 .split('\n')
                 .map((item, index) => ({ id: index, domain: item }));
-            // console.log('Request for domains: ', this.domains);
-            this.items = await request({ domains: this.domains });
-            this.isBusy = false;
+            await processArray(this.domains, this.items, this.progress);
+
+            setTimeout(() => {
+                this.isBusy = false;
+            }, 600);
 
             async function request(data) {
                 let response = await fetch('/domain', {
@@ -37,6 +45,24 @@ new Vue({
                 } else {
                     alert("Ошибка HTTP: " + response.status);
                 }
+            }
+
+            async function processArray(array, items) {
+                let groupedArray = [];
+                let copiedArray = array.slice();
+
+                while (copiedArray.length) {
+                    groupedArray.push(copiedArray.splice(0, 3));
+                }
+
+                for (const item of groupedArray) {
+                    console.log('Request for domains: ', item);
+                    let response = await request({ domains: item });
+                    console.log('response: ', response);
+                    response.forEach(item => items.push(item));
+                    console.log('\nitems: ', items);
+                }
+                console.log('Done for all domains!!!');
             }
         }
     }
